023c774ef90bf3b033422a6c9c0be5e849255c39
Alex Sirota <alex.sirota@icloud.com>
2024-07-18 16:01:44 -0700

    init

---
+++ candy_dispenser.ino
@@ -0,0 +1,20 @@
+// Include libraries
+#include <Stepper.h>
+#include <Ultrasonic.h>
+
+// Define motor and sensor
+// Arguments: steps per rotation and pins for each phase
+Stepper motor(200, 2, 3, 4, 5);
+// Arguments: trigger pin, recieve pin
+Ultrasonic sensor(12, 13);
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
